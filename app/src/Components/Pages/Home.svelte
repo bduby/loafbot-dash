eb9659f6f5382261da8fbed93bf646db032cebbd
LoafOfCode <sd.coolkid2@gmail.com>
2021-05-29 03:51:14 -0400

    started work on mainPage

--- app/src/Components/Pages/Home.svelte
+++ app/src/Components/Pages/Home.svelte
@@ -3,14 +3,57 @@
 </script>
 
 <main>
-    <div>
+    <div class="wrapper">
         <h1>LoafBot</h1>
+        <h2>Discord Economy Bot</h2>
+        <h2>With Bread</h2>
+        <div class="content-block">
+            <h2>Fishing System</h2>
+            <img src="\resources\images\fish-loaf.png" alt="Loaf of bread w/rod" width=180px height=180px>
+        </div>
+        <div class="content-block">
+            <h2>Bank System</h2>
+            <img src="\resources\images\loaf-piggy-bank.png" alt="Piggy Bank Bread" width=180px height=180px>
+        </div>
     </div>
 </main>
 
 <style>
-    h1 {
-        color:whitesmoke;
-        margin: 24px;
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap');
+    *{
+        font-family: 'Noto Sans JP', sans-serif;
+    }
+    .wrapper h1{
+
+        color: whitesmoke;
+        margin: 5px;
+    }
+    .wrapper h2{
+    color: whitesmoke;
+    margin: 1px;
+    padding: 0;
+}
+    .wrapper{
+        text-align: center;
+    }
+    .content-block img{
+        display:inline;
+        border-radius: 75px;
+        border-radius: 27px 27px 27px 27px;
+        -moz-border-radius: 27px 27px 27px 27px;
+        -webkit-border-radius: 27px 27px 27px 27px;
+        border: 1px solid #000000;
+    }
+    .content-block{
+        padding-top: 50px;
+        padding-right: 30px;
+        padding-left: 30px;
+        display: inline-block;
+    }
+    img{ 
+        image-rendering: pixelated;
+        image-rendering: -moz-crisp-edges;
+        image-rendering: crisp-edges;
+
     }
 </style>
